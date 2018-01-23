3850d47134857fcedb32335277a5df5397d96e90
David Ladowitz <david@ladowitz.com>
2018-01-22 18:35:18 -0800

    create search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
+const API_KEY = process.env.YOUTUBE_API_KEY
 
 // Create a new component that produces html.
 const App = () => {
-  return <div>Jello World!</div>;
+
+  return <SearchBar />;
 }
 
 // Push component's HTML into the DOM.
